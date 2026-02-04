f304012939fd4112e03974e0e9d6f1b99280f07b
thejosephstevens <thejosephstevens@gmail.com>
2026-02-04 11:30:16 -0800

    add ty and ruff to python

---
+++ python/completion.sh
@@ -0,0 +1,4 @@
+eval "$(uv generate-shell-completion zsh)"
+eval "$(uvx --generate-shell-completion zsh)"
+eval "$(ruff generate-shell-completion zsh)"
+eval "$(ty generate-shell-completion zsh)"
--- python/install.sh
+++ python/install.sh
@@ -2,3 +2,19 @@ if ! command -v uv > /dev/null 2>&1; then
   echo "» Installing uv"
   curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
+
+uv self update
+
+if ! command -v ruff > /dev/null 2>&1; then
+  echo "» Installing ruff"
+  uv tool install ruff@latest
+fi
+
+uv tool upgrade ruff
+
+if ! command -v ty > /dev/null 2>&1; then
+  echo "» Installing ty"
+  uv tool install ty@latest
+fi
+
+uv tool upgrade ty
